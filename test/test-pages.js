2147ca69caa5a30e811a4cfa8b0eaaf6dd6274a4
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-29 02:04:33 +0500

    new

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,7 +1,15 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
+class Vehicle {
+    speed = 80;
+    constructor(type) {
+      this.type = type;
+    }
+}
+
 it('Main page content', function(done) {
+    var v = new Vehicle("test");
     request('http://localhost:8080' , function(error, response, body) {
         expect(body).to.equal('Hello World');
         done();
