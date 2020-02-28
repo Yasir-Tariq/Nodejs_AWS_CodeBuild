901e505bc797df7803cf58dc2f7c4ef2e21de913
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-29 02:57:58 +0500

    new

--- test/test-pages.js
+++ test/test-pages.js
@@ -25,12 +25,13 @@ var request = require('request');
 const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
 var assert = require('assert');
 it('should throw error when the version is not valid', function() {
-    if (assert(NODE_MAJOR_VERSION > 12)) {
-        console.log("OK!");
-    }
-    else{
-        throw new Error('Requires Node 13 (or higher)');
-    }
+    assert(NODE_MAJOR_VERSION > 12);
+    // if (assert(NODE_MAJOR_VERSION > 12)) {
+    //     console.log("OK!");
+    // }
+    // else{
+    //     throw new Error('Requires Node 13 (or higher)');
+    // }
     // assert.equal(NODE_MAJOR_VERSION, 13);
     // throw new Error('Requires Node 13 (or higher)');
   });
