b7fa67dea002c2c55cff11949cb7d8fcafb4f810
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-29 02:19:50 +0500

    new

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,17 +1,23 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
-class Vehicle {
-    speed = 80;
-    constructor(type) {
-      this.type = type;
-    }
-}
+// class Vehicle {
+//     speed = 80;
+//     constructor(type) {
+//       this.type = type;
+//     }
+// }
 
-it('Main page content', function(done) {
-    var v = new Vehicle("test");
-    request('http://localhost:8080' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
-        done();
-    });
-});
+// it('Main page content', function(done) {
+//     var v = new Vehicle("test");
+//     // request('http://localhost:8080' , function(error, response, body) {
+//     //     expect(body).to.equal('Hello World');
+//     //     done();
+//     // });
+// });
+
+
+const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
+if (NODE_MAJOR_VERSION < 13) {
+  throw new Error('Requires Node 12 (or higher)');
+}
