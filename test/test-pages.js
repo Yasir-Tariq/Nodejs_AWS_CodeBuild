9bc3c6c7c72d69f0a4145883e0ad12a1eba5899e
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-29 03:02:10 +0500

    new

--- test/test-pages.js
+++ test/test-pages.js
@@ -22,19 +22,19 @@ var request = require('request');
 // }
 
 
-const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
-var assert = require('assert');
-it('should throw error when the version is not valid', function() {
-    assert(NODE_MAJOR_VERSION > 12);
-    // if (assert(NODE_MAJOR_VERSION > 12)) {
-    //     console.log("OK!");
-    // }
-    // else{
-    //     throw new Error('Requires Node 13 (or higher)');
-    // }
-    // assert.equal(NODE_MAJOR_VERSION, 13);
-    // throw new Error('Requires Node 13 (or higher)');
-  });
+// const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
+// var assert = require('assert');
+// it('should throw error when the version is not valid', function() {
+//     assert(NODE_MAJOR_VERSION > 12);
+//     // if (assert(NODE_MAJOR_VERSION > 12)) {
+//     //     console.log("OK!");
+//     // }
+//     // else{
+//     //     throw new Error('Requires Node 13 (or higher)');
+//     // }
+//     // assert.equal(NODE_MAJOR_VERSION, 13);
+//     // throw new Error('Requires Node 13 (or higher)');
+//   });
 
 // var assert = require('assert');
 // describe('Array', function() {
@@ -44,3 +44,15 @@ it('should throw error when the version is not valid', function() {
 //     });
 //   });
 // });
+
+
+
+const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([10,11,12,13].indexOf(NODE_MAJOR_VERSION), -1);
+    });
+  });
+});
