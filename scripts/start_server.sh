a3e7f962a90b613444bb407f7e26ff0cecab4f15
Yasir-Tariq <ynaseem78@gmail.com>
2020-03-03 03:40:39 +0500

    new

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/myapp
 npm install express
-# node index.js
 forever start app.js
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/myapp
+npm install forever
 forever stopall
