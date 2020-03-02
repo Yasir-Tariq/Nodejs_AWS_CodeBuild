fcc19b73fdf17b82de0628ec11d4d98805c743ff
Yasir-Tariq <ynaseem78@gmail.com>
2020-03-03 04:56:03 +0500

    new

--- test/test-pages.js
+++ test/test-pages.js
@@ -4,7 +4,7 @@ console.log(process.versions);
 // const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
 var assert = require('assert');
 it('should throw error when the version is not valid', function() {
-    if (NODE_MAJOR_VERSION >= 12)
+    if (NODE_MAJOR_VERSION >= 11)
         assert.ok(true, "Node version is compatible");
     else
         assert.ok(false, "Node version is not compatible");
