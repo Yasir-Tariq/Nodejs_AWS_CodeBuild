b5f97d7ddc6a7ffd8da66bfb6f6a083e7b4764e4
Yasir-Tariq <ynaseem78@gmail.com>
2020-03-03 03:22:15 +0500

    new

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,26 +1,3 @@
-// var expect  = require('chai').expect;
-// var request = require('request');
-
-// class Vehicle {
-//     speed = 80;
-//     constructor(type) {
-//       this.type = type;
-//     }
-// }
-
-// it('Main page content', function(done) {
-//     var v = new Vehicle("test");
-//     // request('http://localhost:8080' , function(error, response, body) {
-//     //     expect(body).to.equal('Hello World');
-//     //     done();
-//     // });
-// });
-
-
-// if (NODE_MAJOR_VERSION < 13) {
-//   throw new Error('Requires Node 12 (or higher)');
-// }
-
 const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
 console.log(NODE_MAJOR_VERSION);
 console.log(process.versions);
@@ -32,30 +9,3 @@ it('should throw error when the version is not valid', function() {
     else
         assert.ok(false, "Node version is not compatible");
 });
-
-// var assert = require('assert');
-// describe('Array', function() {
-//   describe('#indexOf()', function() {
-//     it('should return -1 when the version is not valid', function() {
-//       assert(NODE_MAJOR_VERSION > 12, -1);
-//     });
-//   });
-// });
-
-// const NODE_MAJOR_VERSION = 22;
-// // const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
-// console.log(NODE_MAJOR_VERSION);
-// var assert = require('assert');
-// describe('Array', function() {
-//   describe('#indexOf()', function() {
-//     it('Node Version Check', function() {
-//       assert.equal([10,11,12,13].indexOf(NODE_MAJOR_VERSION), -1);
-//     });
-//   });
-// });
-
-
-
-// // const NODE_MAJOR_VERSION = 22;
-// // var assert = require('assert');
-// // assert.equal(22, NODE_MAJOR_VERSION, "My message goes here");
